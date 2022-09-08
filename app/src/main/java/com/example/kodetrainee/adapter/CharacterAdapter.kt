7036bd912d75f036153add0adb7955602d77b654
NoOseGG <hoc751@gmail.com>
2022-09-08 16:06:03 +0300

    created CharacterAdapter and library Coil
    added CharacterAdapter.kt, item_character.xml,

---
+++ app/src/main/java/com/example/kodetrainee/adapter/CharacterAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.kodetrainee.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.domain.model.Character
+import com.example.kodetrainee.databinding.ItemCharacterBinding
+
+class CharacterAdapter : ListAdapter<Character, CharacterViewHolder>(DIFF_UTIL) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        return CharacterViewHolder(
+            ItemCharacterBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val DIFF_UTIL = object : DiffUtil.ItemCallback<Character>() {
+            override fun areItemsTheSame(oldItem: Character, newItem: Character): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Character, newItem: Character): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+class CharacterViewHolder(
+    private val binding: ItemCharacterBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: Character) {
+        with(binding) {
+            imgAvatar.load(item.image)
+            tvCharacterName.text = item.name
+        }
+    }
+}
